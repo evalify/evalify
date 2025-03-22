4a0a705dd07f7266aa12e955497ba4cdd401af98
Aksay kanthan <aksaykanthan@gmail.com>
2025-03-22 13:17:45 +0530

    added: Logger

---
+++ src/lib/logger/loki.ts
@@ -0,0 +1,34 @@
+import 'server-only';
+import { createLogger, transports, format } from "winston";
+import LokiTransport from "winston-loki";
+
+const loki_url = process.env.LOKI_URL;
+
+let logger;
+
+if (loki_url) {
+  logger = createLogger({
+    level: "info",
+    format: format.json(),
+    transports: [
+      new LokiTransport({
+        host: loki_url,
+        labels: { app: "nextjs" },
+        json: true,
+        batching: true,
+      }),
+      new transports.Console(),
+    ],
+  });
+} else {
+  logger = createLogger({
+    level: "info",
+    format: format.json(),
+    transports: [
+      new transports.Console(),
+    ],
+  });
+  console.warn("LOKI_URL environment variable is not set, using console transport only");
+}
+
+export default logger;
