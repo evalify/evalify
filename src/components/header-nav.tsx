f3ea99532ba0733da65fc546e687d75d4e503ee9
Aksay kanthan <aksaykanthan@gmail.com>
2025-03-22 13:18:23 +0530

    enhanced: removed blur effect

--- src/components/header-nav.tsx
+++ src/components/header-nav.tsx
@@ -10,7 +10,7 @@ const HeaderNavbar = async () => {
     const session = await auth();
 
     return (
-        <header className="fixed top-0 left-0 right-0 h-14 border-b z-30 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="fixed top-0 left-0 right-0 h-14 border-b z-30 bg-background/95 supports-[backdrop-filter]:bg-background/60">
             <div className='flex items-center justify-between h-full px-4'>
                 <Link href='/' className='flex items-center gap-2'>
                     <p className='rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-x1 font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white'>
