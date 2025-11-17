96e855443489e630c1a280bbcfedf187fab6c3eb
Manasa V <manuvenke06@gmail.com>
2025-11-17 21:44:42 +0530

    fix(#6): fixed explanation in the questions

--- src/app/api/eval/quiz/[quizId]/question/[questionId]/route.ts
+++ src/app/api/eval/quiz/[quizId]/question/[questionId]/route.ts
@@ -31,6 +31,8 @@ export async function GET(
                 bloomTaxonomyLevel: questionsTable.bloomTaxonomyLevel,
                 question: questionsTable.question,
                 questionData: questionsTable.questionData,
+                explanation: questionsTable.explanation,
+                solution: questionsTable.solution,
                 createdById: questionsTable.createdById,
                 created_at: questionsTable.created_at,
                 updated_at: questionsTable.updated_at,
