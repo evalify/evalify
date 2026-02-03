00cda60a1208eaa345fb2f539f60a20a17bea202
Aksay Kanthan <aksaykanthan@gmail.com>
2026-02-03 13:30:06 +0530

    chore: drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,20 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+const url = new URL(process.env.DATABASE_URL || "");
+
 export default defineConfig({
     out: "./drizzle",
     schema: "./src/db/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
-        url:
-            process.env.DATABASE_URL ||
-            (() => {
-                throw new Error("DATABASE_URL environment variable is required");
-            })(),
+        host: url.hostname,
+        port: parseInt(url.port || "5432"),
+        user: url.username,
+        password: url.password,
+        database: url.pathname.slice(1),
+        ssl: {
+            rejectUnauthorized: false,
+        },
     },
 });
